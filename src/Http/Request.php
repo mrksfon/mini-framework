3033fdcf33891f95ce245b2c1070fee70f024b39
Marko <markostevic96@hotmail.com>
2026-07-28 18:35:14 +0200

    feat(http): store request path (#66)

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,10 +6,15 @@ namespace Framework\Http;
 
 final class Request
 {
-    public function __construct(private string $method) {}
+    public function __construct(private string $method, private string $path) {}
 
     public function method(): string
     {
         return $this->method;
+    }
+
+    public function path(): string
+    {
+        return $this->path;
     }
 }
--- tests/Unit/RequestTest.php
+++ tests/Unit/RequestTest.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 use Framework\Http\Request;
 
 it('can create a request object', function () {
-    $request = new Request('GET');
+    $request = new Request('GET', '/users');
 
     expect($request)->toBeInstanceOf(Request::class);
 });
 
 it('stores the http method', function () {
-    $request = new Request('GET');
+    $request = new Request('GET', '/users');
 
     expect($request->method())->toBe('GET');
+});
+
+it('stores the request path', function () {
+    $request = new Request('GET', '/users');
+
+    expect($request->path())->toBe('/users');
 });
