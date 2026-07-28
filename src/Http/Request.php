2e823853824b8aed9af3fe762766629f5eb13cbe
mrksfon <markostevic96@hotmail.com>
2026-07-28 18:21:20 +0200

    feat(http): store request method

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 namespace Framework\Http;
 
-final class Request {}
+final class Request
+{
+    public function __construct(private string $method) {}
+
+    public function method(): string
+    {
+        return $this->method;
+    }
+}
--- tests/Unit/RequestTest.php
+++ tests/Unit/RequestTest.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 use Framework\Http\Request;
 
 it('can create a request object', function () {
-    $request = new Request;
+    $request = new Request('GET');
 
     expect($request)->toBeInstanceOf(Request::class);
+});
+
+it('stores the http method', function () {
+    $request = new Request('GET');
+
+    expect($request->method())->toBe('GET');
 });
